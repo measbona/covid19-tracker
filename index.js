18e9844567d7c8cd0559213a11b928c37078a862
measbona <47500564+measbona@users.noreply.github.com>
2021-03-25 18:51:45 +0700

    remove credentials and disable schedule

--- index.js
+++ index.js
@@ -1,109 +1,92 @@
-const schedule = require('node-schedule');
-const nodemailer = require("nodemailer");
-const { covidUpdate } = require('./covidUpdate')
-const covidData = require('./db.json')
-const _ = require('lodash')
+// const schedule = require('node-schedule');
+// const nodemailer = require("nodemailer");
+// const { covidUpdate } = require('./covidUpdate')
+// const { covidAmount } = require('./covidAmount')
+// const covidData = require('./db.json')
+// const _ = require('lodash')
 
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'bonameas3@gmail.com',
-    pass: 'Meas@1234',
-  },
-})
+// const mailTransport = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'username',
+//     pass: 'userpassword',
+//   },
+// })
 
-covidTable = () => {
-  let htmlData = ``
-  let infectedCount = 0
-  let recoveredCount = 0
-  let deadCount = 0
+// covidTable = () => {
+//   let htmlData = ``
+//   let infectedCount = 0
+//   let recoveredCount = 0
+//   let deadCount = 0
 
-  return new Promise((resolve) => {
-    _.forEach(covidData.region_infection, province => {
-      infectedCount += province.infected_count
-      recoveredCount += province.recovered_count
-      deadCount += province.dead_count
+//   return new Promise((resolve) => {
+//     _.forEach(covidData.region_infection, province => {
+//       infectedCount += province.infected_count
+//       recoveredCount += province.recovered_count
+//       deadCount += province.dead_count
 
-      htmlData += `
-        <tr>
-          <td>${province.place_name}</td>
-          <td>${province.infected_count}</td>
-          <td>${province.recovered_count}</td>
-          <td>${province.dead_count}</td>
-        </tr>
-      `
-    })
+//       htmlData += `
+//         <tr>
+//           <td>${province.place_name}</td>
+//           <td>${province.infected_count}</td>
+//           <td>${province.recovered_count}</td>
+//           <td>${province.dead_count}</td>
+//         </tr>
+//       `
+//     })
 
-    htmlData += `
-      <tr>
-        <td>Amount</td>
-        <td>${infectedCount}</td>
-        <td>${recoveredCount}</td>
-        <td>${deadCount}</td>
-      </tr>
-    `
+//     htmlData += `
+//       <tr>
+//         <td>Amount</td>
+//         <td>${infectedCount}</td>
+//         <td>${recoveredCount}</td>
+//         <td>${deadCount}</td>
+//       </tr>
+//     `
 
-    htmlData += `
-      <tr>
-        <td>Total</td>
-        <td>${infectedCount + recoveredCount + deadCount}</td>
-      </tr>
-    `
+//     htmlData += `
+//       <tr>
+//         <td>Total</td>
+//         <td>${infectedCount + recoveredCount + deadCount}</td>
+//       </tr>
+//     `
 
-    resolve(htmlData)
-  })
-}
+//     resolve(htmlData)
+//   })
+// }
 
-mailSender = async (error = false) => {
-  const table = await covidTable()
+// mailSender = async (error = false) => {
+//   const table = await covidTable()
 
-  await mailTransport.sendMail({
-    from: `Covid Update <noreply@firebase.com>`,
-    to: 'measbona3@gmail.com',
-    subject: 'Covid Table',
-    html: `
-    ${!error ? `
-      <h3>Covid Update at ${new Date()}</h3>
-      <table style="width:100%">
-      <tr>
-        <th>Province</th>
-        <th>Infected</th> 
-        <th>Recovered</th>
-        <th>Dead</th>
-      </tr>
-        ${table}
-      </table>
-    ` : 
-    `
-      <h4>At : ${new Date()}</h4>
-      <h2>Error on : ${error}</h2>
-    `}`
-  })
-}
+//   await mailTransport.sendMail({
+//     from: `Covid Update <noreply@firebase.com>`,
+//     to: 'measbona3@gmail.com',
+//     subject: 'Covid Table',
+//     html: `
+//     ${!error ? `
+//       <h3>Covid Update at ${new Date()}</h3>
+//       <table style="width:100%">
+//       <tr>
+//         <th>Province</th>
+//         <th>Infected</th> 
+//         <th>Recovered</th>
+//         <th>Dead</th>
+//       </tr>
+//         ${table}
+//       </table>
+//     ` : 
+//     `
+//       <h4>At : ${new Date()}</h4>
+//       <h2>Error on : ${error}</h2>
+//     `}`
+//   })
+// }
 
-// schedule.scheduleJob('0 10 * * *', async () => {
+// schedule.scheduleJob('* * * * *', async () => {
 //   try {
-//     // await covidUpdate()
-//     await mailSender()
+//     //
 //   } catch (err) {
 //     await mailSender(err.message)
 //   }
 // });
-
-// schedule.scheduleJob('0 13 * * *', async () => {
-//   try {
-//     // await covidUpdate()
-//     await mailSender()
-//   } catch (err) {
-//     await mailSender(err.message)
-//   }
-// });
-
-schedule.scheduleJob('47 18 * * *', async () => {
-  try {
-    // await covidUpdate()
-    await mailSender()
-  } catch (err) {
-    await mailSender(err.message)
-  }
-});
+console.log('Start')
